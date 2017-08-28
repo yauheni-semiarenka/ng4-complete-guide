1b3a3a78527ce75032bc028fe3c623ae85074bd6
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-08-28 14:52:18 +0300

    Added dropdown logic

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, HostBinding } from '@angular/core';
 import {Recipe} from '../../recipe.model';
 
 @Component({
@@ -9,6 +9,7 @@ import {Recipe} from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Output() recipeSelected = new EventEmitter<void>();
+  @HostBinding('class') selectedStyle = '';
 
   constructor() { }
 
@@ -17,5 +18,10 @@ export class RecipeItemComponent implements OnInit {
 
   onSelected() {
     this.recipeSelected.emit();
+    if (this.selectedStyle) {
+      this.selectedStyle = '';
+    } else {
+      this.selectedStyle = 'selected';
+    }
   }
 }
