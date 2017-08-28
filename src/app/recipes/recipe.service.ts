2253fc010b3bdbbe7c84cf72b2087ced61a4e1e5
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-08-28 16:52:03 +0300

    Added "Add Ingredients to shopping list" logic

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,39 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'http://www.seriouseats.com/images/' +
-          '2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-        new Recipe('Pizza Recipe', 'This is simply a test', 'http://www.simplyrecipes.com/wp-content/uploads/' +
-          '2007/01/homemade-pizza-horiz-a-1200.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is simply a test', 
+            'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Pizza Recipe', 
+            'This is simply a test', 
+            'http://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         //Just return a copy of recipes array
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
