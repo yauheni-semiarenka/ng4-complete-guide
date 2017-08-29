30042c871a4b5806616d54c4b4b92e1c13979483
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-08-29 17:04:32 +0300

    Added routers logic

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,15 +9,31 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    // Works only when component loads at first time
+    // const id = this.route.snapshot.params['id'];
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route }); equls to code below
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
