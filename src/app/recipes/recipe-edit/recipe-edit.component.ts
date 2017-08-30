57d06c99d36d9fe7e09ebdfe01cd7b07d0c688f5
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-08-30 13:29:49 +0300

    Minor updates and update dev dependencies

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -8,7 +8,7 @@ import {ActivatedRoute, Params} from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
   id: number;
-  editMode: boolean = false;
+  editMode = false;
 
   constructor(private route: ActivatedRoute) { }
 
