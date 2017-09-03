61a544dd90d18ca5345d4c8f20caa8c8e4440010
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-09-03 22:56:28 +0300

    Added auth logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCxpS94Ev17Gg2ajOVYkGnLG4NXe3io80k',
+      authDomain: 'ng-recipe-book-test-project.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
