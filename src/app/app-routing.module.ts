a1b777f21836f2b5a01e849ee3117bfb851dcaaf
Yauheni Semiarenka <yauheni.semiarenka@gmail.com>
2017-09-13 11:35:52 +0300

    Enable preloaded lazy-loadig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HomeComponent } from './core/home/home.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }, // enable lazy loading
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [
+        RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules}) //preload all lazy modules after all modules have been loaded
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
